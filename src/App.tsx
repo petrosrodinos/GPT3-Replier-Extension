d81150db596d9930a53b41955cf286f46fb916a1
Petros Rodinos <petros.rodinos@yahoo.com>
2022-11-17 18:20:59 +0200

    added moc home and contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import AppBar from "./components/AppBar";
 import Auth from "./pages/Auth";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Container from "./components/Container";
 import Settings from "./pages/Settings";
 import Pricing from "./pages/Pricing";
+import ContactForm from "./pages/Contact";
+
 function App() {
   return (
     <>
@@ -15,8 +17,10 @@ function App() {
             <Route path="/*">
               <Route index path="home" element={<Home />} />
               <Route path="login" element={<Auth />} />
+              <Route path="contact" element={<ContactForm />} />
               <Route path="settings" element={<Settings />} />
               <Route path="pricing" element={<Pricing />} />
+              <Route path="*" element={<Navigate to="/home" replace />} />
             </Route>
           </Routes>
         </BrowserRouter>
